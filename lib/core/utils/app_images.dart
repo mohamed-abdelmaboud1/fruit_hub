cbb504101ab0bb6e19017f02f8ba7ffeb1a03c2e
MohamedAbdelmaboud <mohamedabdlmabod03@gmail.com>
2024-08-04 02:56:05 +0300

    first commit

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,56 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesAppIcon
+  /// assets/images/app_icon.png
+  static const String assetsImagesAppIcon = "assets/images/app_icon.png";
+
+  /// Assets for assetsImagesApplIcon
+  /// assets/images/appl_icon.svg
+  static const String assetsImagesApplIcon = "assets/images/appl_icon.svg";
+
+  /// Assets for assetsImagesCheck
+  /// assets/images/Check.svg
+  static const String assetsImagesCheck = "assets/images/Check.svg";
+
+  /// Assets for assetsImagesFacebookIcon
+  /// assets/images/facebook_icon.svg
+  static const String assetsImagesFacebookIcon = "assets/images/facebook_icon.svg";
+
+  /// Assets for assetsImagesGoogleIcon
+  /// assets/images/google_icon.svg
+  static const String assetsImagesGoogleIcon = "assets/images/google_icon.svg";
+
+  /// Assets for assetsImagesLogo
+  /// assets/images/logo.svg
+  static const String assetsImagesLogo = "assets/images/logo.svg";
+
+  /// Assets for assetsImagesPageViewItem1BackgroundImage
+  /// assets/images/page_view_item1_background_image.svg
+  static const String assetsImagesPageViewItem1BackgroundImage = "assets/images/page_view_item1_background_image.svg";
+
+  /// Assets for assetsImagesPageViewItem1Image
+  /// assets/images/page_view_item1_image.svg
+  static const String assetsImagesPageViewItem1Image = "assets/images/page_view_item1_image.svg";
+
+  /// Assets for assetsImagesPageViewItem2BackgroundImage
+  /// assets/images/page_view_item2_background_image.svg
+  static const String assetsImagesPageViewItem2BackgroundImage = "assets/images/page_view_item2_background_image.svg";
+
+  /// Assets for assetsImagesPageViewItem2Image
+  /// assets/images/page_view_item2_image.svg
+  static const String assetsImagesPageViewItem2Image = "assets/images/page_view_item2_image.svg";
+
+  /// Assets for assetsImagesPlant
+  /// assets/images/plant.svg
+  static const String assetsImagesPlant = "assets/images/plant.svg";
+
+  /// Assets for assetsImagesSplashBottom
+  /// assets/images/splash_bottom.svg
+  static const String assetsImagesSplashBottom = "assets/images/splash_bottom.svg";
+
+  /// Assets for assetsImagesVector
+  /// assets/images/Vector.svg
+  static const String assetsImagesVector = "assets/images/Vector.svg";
+}
+
