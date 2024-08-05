d2996b76d8e18c3a6faafec95ffc8c249a402f10
MohamedAbdelmaboud <mohamedabdlmabod03@gmail.com>
2024-08-05 08:54:41 +0300

    feat: Add onboarding view component

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,14 +1,21 @@
 import 'package:fruit_hub/features/splash/ui/view/spalsh_view.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../features/onboarding/ui/view/onboarding_view.dart';
+
 class AppRouter {
   static const String splash = '/';
+  static const String onBoarding = '/onBoarding';
   static final router = GoRouter(
     routes: [
       GoRoute(
         path: splash,
         builder: (context, state) => SpalshView(),
-      )
+      ),
+      GoRoute(
+        path: onBoarding,
+        builder: (context, state) => OnboardingView(),
+      ),
     ],
   );
 }
--- lib/features/splash/ui/view/spalsh_view.dart
+++ lib/features/splash/ui/view/spalsh_view.dart
@@ -1,14 +1,36 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../../core/routing/app_router.dart';
 import '../widgets/splash_view_body.dart';
 
-class SpalshView extends StatelessWidget {
+class SpalshView extends StatefulWidget {
   const SpalshView({Key? key}) : super(key: key);
+
+  @override
+  State<SpalshView> createState() => _SpalshViewState();
+}
+
+class _SpalshViewState extends State<SpalshView> {
+  @override
+  void initState() {
+    super.initState();
+    navigateToOnboarding();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SplashViewBody(),
     );
+  }
+
+  void navigateToOnboarding() {
+    Timer(const Duration(seconds: 2), () {
+      // Navigate to onBoarding after 2 seconds
+      context.go(AppRouter.onBoarding);
+    });
   }
 }
