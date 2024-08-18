3634405474436434bb77e6e2325670d3f898446f
MohamedAbdelmaboud <mohamedabdlmabod03@gmail.com>
2024-08-18 08:05:35 +0300

    feat: Implement user registration functionality

---
+++ lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fruit_hub/features/auth/domain/entites/user_entity.dart';
+
+class UserModel extends UserEntity {
+  UserModel({
+    required super.id,
+    required super.email,
+    required super.name,
+  });
+  factory UserModel.fromFirebaseUser(User user) {
+    return UserModel(
+      id: user.uid,
+      email: user.email ?? '',
+      name: user.displayName ?? '',
+    );
+  }
+}
--- lib/features/auth/data/repos/auth_repo_implmentaion.dart
+++ lib/features/auth/data/repos/auth_repo_implmentaion.dart
@@ -1,13 +1,27 @@
 import 'package:dartz/dartz.dart';
-import '../../../../core/error/failures.dart';
-import '../../domain/entites/user_entity.dart';
 
+import '../../../../core/error/exceptions.dart';
+import '../../../../core/error/failures.dart';
+import '../../../../core/services/firebase_auth_services.dart';
+import '../../domain/entites/user_entity.dart';
 import '../../domain/reops/auth_repo.dart';
+import '../models/user_model.dart';
 
 class AuthRepoimplmentaion extends AuthRepo {
+  final FirebaseAuthServices firebaseAuthServices;
+
+  AuthRepoimplmentaion({
+    required this.firebaseAuthServices,
+  });
   @override
   Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
-      String email, String password) {
-    
+      String email, String password) async {
+    try {
+      final user = await firebaseAuthServices.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return right(UserModel.fromFirebaseUser(user));
+    } on CustomException catch (e) {
+      return left(ServerFailure(message: e.toString()));
+    }
   }
 }
